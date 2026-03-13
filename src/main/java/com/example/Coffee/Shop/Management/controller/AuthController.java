703c44a91bdff2e8c7293eed1bbdc1d9f7b3d251
димас <lafofunfor@gmail.com>
2026-03-13 01:24:24 +0100

    feat: full JWT security flow is working. Secured demo endpoint accessible.

--- src/main/java/com/example/Coffee/Shop/Management/controller/AuthController.java
+++ src/main/java/com/example/Coffee/Shop/Management/controller/AuthController.java
@@ -1,13 +1,11 @@
 package com.example.Coffee.Shop.Management.controller;
 
 import com.example.Coffee.Shop.Management.dto.AuthRequestDto;
+import com.example.Coffee.Shop.Management.dto.AuthResponseDto;
 import com.example.Coffee.Shop.Management.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -19,5 +17,15 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody AuthRequestDto request) {
         return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDto> authenticate(@RequestBody AuthRequestDto request) {
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+
+    @GetMapping("/demo")
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello! You are authenticated with JWT!");
     }
 }
--- src/main/java/com/example/Coffee/Shop/Management/service/AuthService.java
+++ src/main/java/com/example/Coffee/Shop/Management/service/AuthService.java
@@ -1,9 +1,12 @@
 package com.example.Coffee.Shop.Management.service;
 
 import com.example.Coffee.Shop.Management.dto.AuthRequestDto;
+import com.example.Coffee.Shop.Management.dto.AuthResponseDto;
 import com.example.Coffee.Shop.Management.model.user.User;
 import com.example.Coffee.Shop.Management.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,8 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final UserRepository userRepository ;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     public String register(AuthRequestDto request){
 
@@ -23,5 +28,28 @@ public class AuthService {
         userRepository.save(user);
 
         return "User registered successfully!";
+    }
+
+
+
+    public AuthResponseDto authenticate(AuthRequestDto request) {
+        // 1. Проверяем логин и пароль (если неверно — вылетит исключение)
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        var user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow();
+
+
+        var jwtToken = jwtService.generateToken(user);
+
+
+        return AuthResponseDto.builder()
+                .token(jwtToken)
+                .build();
     }
 }
